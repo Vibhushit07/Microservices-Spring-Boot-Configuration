844fcdca154f33be0ac85f05b707ea695ae7cfd9
Vibhushit07 <vibhushitsinghal80@gmail.com>
2023-01-17 23:03:31 +0530

    value annotation

--- src/main/java/io/javabrains/springbootconfig/GeetingController.java
+++ src/main/java/io/javabrains/springbootconfig/GeetingController.java
@@ -4,14 +4,29 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class GeetingController {
 
     @Value("${my.greeting}")
     private String greetingMessage;
 
+    @Value("${my.greetings: default value}")
+    private String greetingsMessage;
+
+    @Value("some static message")
+    private String staticMessage;
+
+    @Value("${my.list.values}")
+    private List<String> listValues;
+
+    @Value("#{${dbValues}}")
+    private Map<String, String> dbValues;
+
     @GetMapping("/greeting")
     public String greeting() {
-        return greetingMessage;
+        return greetingMessage + " " + greetingsMessage + " " + staticMessage + " " + listValues + " " + dbValues;
     }
 }
